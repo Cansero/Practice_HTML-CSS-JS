f95db5515bebc9c2f9147e9b271a2243da114078
Cansero <ivanmena0011@gmail.com>
2023-08-01 16:33:34 -0400

    Form and event listeners ready

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 const nick = document.getElementById('nick');
+const email = document.getElementById('email');
 const size = document.getElementById('size');
 const forminput = document.getElementById('forminput');
 const error = document.getElementById('error');
+const avataritems = document.getElementsByClassName('avatarimgitem');
+const avatarcontainer = document.getElementById('avatarcontainer');
+const avatarimg = document.getElementById('avatarimg');
+let imgdrop;
 
 if (getdata('error')) {
     error.innerText = getdata('error')
@@ -13,13 +18,41 @@ function formtest(event) {
         event.preventDefault();
         error.innerText = 'NO NICK NAME';
         nick.focus();
+        return false;
     } else if (size.value === '0') {
         event.preventDefault();
         error.innerText = 'NO SIZE SELECTED';
         size.focus();
+        return false;
     }
     setdata(nick.value, 'nick');
+    if (email.value) {
+        setdata(email.value, 'email');
+    }
+    setdata(size.value, 'size');
+    setdata(avatarimg.src, 'avatar')
     savehistory(nick.value);
 }
 
+function imgmov(event) {
+    imgdrop = event.target;
+}
+
+function setimg() {
+    avatarimg.src = imgdrop.src;
+}
+
+// Main
 forminput.addEventListener('submit', formtest);
+
+for (let avataritem of avataritems) {
+    avataritem.addEventListener('dragstart', imgmov);
+    avataritem.addEventListener('click', event => {
+        avatarimg.src = event.target.src;
+    });
+}
+
+avatarcontainer.addEventListener('dragover', event => {
+    event.preventDefault();
+});
+avatarcontainer.addEventListener('drop', setimg)
