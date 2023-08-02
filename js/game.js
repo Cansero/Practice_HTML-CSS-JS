19280dcb3ad3ac081295963f2c371ba6726a0c8e
Cansero <131320384+Cansero@users.noreply.github.com>
2023-08-02 00:15:21 -0400

    Add game style and js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,22 @@
-let nick = getdata('nick');
+let usernick = getdata('nick');
+let gamesize = getdata('size');
+let avatarsrc = getdata('avatar');
+
+let nick = document.getElementById('nick');
+let avatar = document.getElementById('avatar');
+let game = document.getElementById('game');
+
 if (nick === null) {
     setdata('NO PREVIOUS SESSION SAVED', 'error');
     window.location = 'index.html'
 }
+
+function makepanel() {
+    game.style.gridTemplateColumns = 'repeat(' + gamesize + ', 1fr';
+    game.style.gridTemplateRows = 'repeat(' + gamesize + ', 1fr';
+}
+
+nick.value = usernick;
+avatar.src = avatarsrc;
+
+makepanel();
