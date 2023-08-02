04d2f90dc4a843cf06c5386899111707203980f0
Cansero <ivanmena0011@gmail.com>
2023-08-02 15:59:28 -0400

    Panel ready

--- js/game.js
+++ js/game.js
@@ -6,14 +6,27 @@ let nick = document.getElementById('nick');
 let avatar = document.getElementById('avatar');
 let game = document.getElementById('game');
 
-if (nick === null) {
+if (usernick === null) {
     setdata('NO PREVIOUS SESSION SAVED', 'error');
     window.location = 'index.html'
+}
+
+function randintpick(max) {
+    return Math.floor(Math.random() * max);
 }
 
 function makepanel() {
     game.style.gridTemplateColumns = 'repeat(' + gamesize + ', 1fr';
     game.style.gridTemplateRows = 'repeat(' + gamesize + ', 1fr';
+
+    let items = '';
+    let colors = ['red', 'green'];
+    let color = colors[randintpick(colors.length)];
+    for (let i = 0; i < (parseInt(gamesize)**2); i++) {
+        if (i%2>0) color =  colors[randintpick(colors.length)];
+        items += `<div class="containeritem"><div class="item ${color}"></div></div>`;
+    }
+    game.innerHTML = items;
 }
 
 nick.value = usernick;
